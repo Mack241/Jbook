9565598d6b403e5b28a35376ca0f52ae8c4e3fd5
Mayukh Dasgupta <mayukhdasgupta18@gmail.com>
2022-02-17 21:17:27 +0530

    Jbook initial commit

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import CodeEditor from './code-editor'
+import Preview from './preview'
+import bundler from '../bundler'
+import Resizable from './resizable'
+
+const CodeCell = () => {
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+  const [err, setErr] = useState('')
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundler(input)
+      setCode(output.code)
+      setErr(output.err)
+    }, 1000)
+    
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [input])
+
+  return (
+    <Resizable direction='vertical'>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction='horizontal'>
+          <CodeEditor 
+            initialValue="console.log('Hey there! Lets start coding')"
+            onChange={(value) => setInput(value)}
+            />
+        </Resizable>
+        <Preview code={code} err={err}/>
+      </div>
+  </Resizable>
+  )
+}
+
+export default CodeCell
---
+++ src/components/code-editor.tsx
@@ -0,0 +1,69 @@
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react'
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
+import { useRef } from 'react'
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import './code-editor.css'
+
+interface CodeEditorProps  {
+  initialValue: string
+  onChange(value: string): void 
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>()
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor
+      monacoEditor.onDidChangeModelContent(() => {
+        onChange(getValue())
+      })
+
+      monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
+  }
+
+  const onFormatClick = () => {
+    // get current value
+    const unformatted = editorRef.current.getModel().getValue()
+
+    // format current value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true
+    }).replace(/\n$/, '')
+
+    // set formatted value to the editor
+    editorRef.current.setValue(formatted)
+  }
+
+  return (
+    <div className='editor-wrapper'>
+      <button 
+        className='button button-format is-primary is-small' 
+        onClick={onFormatClick}
+      >Format</button>
+      <MonacoEditor
+          editorDidMount={onEditorDidMount}
+          value={initialValue}
+          language='javascript'
+          theme='dark'
+          options={{
+            wordWrap: 'on',
+            minimap: { enabled: false },
+            showUnused: false,
+            folding: false,
+            lineNumbersMinChars: 3,
+            fontSize: 16,
+            scrollBeyondLastLine: false,
+            automaticLayout: true
+          }}
+          height="100%"
+          />
+    </div>
+  )
+}
+
+export default CodeEditor
